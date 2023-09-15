c5ca2ed10bc36469f36864d26e01a60d18ac73d8
DiamondCoder <diamond.coder.backup@gmail.com>
2023-09-15 11:27:38 +0200

    FUCK YOU

---
+++ demo/src/main/java/com/uni_testing/demo/Car.java
@@ -0,0 +1,40 @@
+package com.uni_testing.demo;
+
+public class Car {
+	private long id;
+	private String brand;
+	private String model;
+	private String year;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long value) {
+		this.id = value;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String value) {
+		this.brand = value;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String value) {
+		this.model = value;
+	}	
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String value) {
+		this.year = value;
+	}
+}
