4d11743d44c5dfae3c6226a30da36c3b183c9687
DiamondCoder <diamond.coder.backup@gmail.com>
2023-09-22 10:13:07 +0200

    fuckyou

--- demo/src/main/java/com/uni_testing/demo/Car.java
+++ demo/src/main/java/com/uni_testing/demo/Car.java
@@ -5,36 +5,29 @@ public class Car {
 	private String brand;
 	private String model;
 	private String year;
-
 	public long getId() {
 		return id;
 	}
-
-	public void setId(long value) {
-		this.id = value;
+	public void setId(long id) {
+		this.id = id;
 	}
-
 	public String getBrand() {
 		return brand;
 	}
-
-	public void setBrand(String value) {
-		this.brand = value;
+	public void setBrand(String brand) {
+		this.brand = brand;
 	}
-
 	public String getModel() {
 		return model;
 	}
-
-	public void setModel(String value) {
-		this.model = value;
-	}	
-
+	public void setModel(String model) {
+		this.model = model;
+	}
 	public String getYear() {
 		return year;
 	}
-
-	public void setYear(String value) {
-		this.year = value;
+	public void setYear(String year) {
+		this.year = year;
 	}
+
 }
