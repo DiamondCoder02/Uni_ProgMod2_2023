ad2d48090f928b0720008480c4fa9b6025dd7c51
DiamondCoder <diamond.coder.backup@gmail.com>
2023-10-13 10:58:10 +0200

    -

--- lesson6/src/main/java/com/lesson6/exception/GlobalExceptionHandler.java
+++ lesson6/src/main/java/com/lesson6/exception/GlobalExceptionHandler.java
@@ -1,11 +1,8 @@
 package com.lesson6.exception;
 
-import com.lesson6.ErrorResponse;
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
